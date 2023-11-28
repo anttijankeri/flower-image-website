a1e39b71c7fe02e57e7a8d4bd59b4be0e00afef0
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-28 09:31:58 +0100

    feat: basic file uploading test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { FormEvent } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const handleSubmit = async (event: FormEvent) => {
+    try {
+      event.preventDefault();
+      const data = new FormData(event.target as HTMLFormElement);
+      const result = await fetch("http://localhost:3456/api/v1/images", {
+        method: "POST",
+        body: data,
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div>WORK IN PROGRESS</div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          name="image"
+          accept="image/png, image/jpg, image/bmp, image/webp"
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
-}
+};
 
 export default App;
