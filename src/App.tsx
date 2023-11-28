a312f73b5302873a45d815cb5a0737f51232c07c
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-28 22:54:31 +0100

    feat: working image saving

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ const App = () => {
   const handleSubmit = async (event: FormEvent) => {
     try {
       event.preventDefault();
-      const data = new FormData(event.target as HTMLFormElement);
+
+      const target = event.target as HTMLFormElement;
+      const data = new FormData(target);
+
+      const active = target.shared.value === "on" ? "true" : "";
+      data.set("shared", active);
+
       const result = await fetch("http://localhost:3456/api/v1/images", {
         method: "POST",
         body: data,
@@ -25,7 +31,10 @@ const App = () => {
           name="image"
           accept="image/png, image/jpg, image/bmp, image/webp"
         />
-        <input type="text" name="test" />
+        <input type="text" name="userText" placeholder="userText" />
+        <input type="text" name="userGroup" placeholder="userGroup" />
+        <input type="text" name="objectLink" placeholder="objectLink" />
+        <input type="checkbox" name="shared" />
         <input type="submit" value="Submit" />
       </form>
     </>
