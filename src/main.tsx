ec4f79bc8017bd82ca127fcf29b11f6bf46d6b50
Antti Jankeri <antti.jankeri@gmail.com>
2023-12-07 08:51:15 +0100

    feat: add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import AuthProvider from "./components/Auth0/AuthProvider";
 import ContextProvider from "./components/Context/ContextProvider";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import Testing from "./Testing";
@@ -11,8 +12,10 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <ContextProvider>
-      <App />
-      <Testing />
+      <BrowserRouter>
+        <App />
+        <Testing />
+      </BrowserRouter>
     </ContextProvider>
   </AuthProvider>
 );
