64faae8da9693531b73d3b0115d8bdd78c342efc
Antti Jankeri <antti.jankeri@gmail.com>
2023-12-07 08:52:48 +0100

    lil refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import AuthProvider from "./components/Auth0/AuthProvider";
 import ContextProvider from "./components/Context/ContextProvider";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import Testing from "./Testing";
